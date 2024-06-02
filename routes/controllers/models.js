9a6808f31541e88bcede58412e48d283390b45e9
yenderjose <167598978+yenderjose@users.noreply.github.com>
2024-06-01 21:04:12 -0400

    Add files via upload

--- routes/controllers/models.js
+++ routes/controllers/models.js
@@ -1,45 +1,111 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require('path');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-class ContactosModel{
-    constructor(){
-      this.db = new sqlite3.Database(path.join(__dirname, "/database", "database.db"), (err) => {
-        if(err){
-          console.log('Error');
+
+
+
+class ContactosModel {
+  constructor() {
+    this.db = new sqlite3.Database(path.join(__dirname, "/database", "database.db"), (err) => {
+      if (err) {
+        console.log('Error');
+      }
+      console.log('Database created successfully')
+    });
+  }
+
+  conectar() {
+    this.db.run('CREATE TABLE IF NOT EXISTS contactos(name VARCHAR(255), email VARCHAR(255), commentary TEXT,ip TEXT,date TEXT,country TEXT)');
+  }
+
+  guardar(nombre, correo, comentario, ip, fecha, pais) {
+    this.db.run("INSERT INTO contactos VALUES (?, ?, ?, ?, ?, ?)", [nombre, correo, comentario, ip, fecha, pais]);
+  }
+
+}
+
+
+class ContactosController {
+  constructor() {
+    this.model = new ContactosModel();
+    this.model.conectar();
+  }
+
+  async save(req, res) {
+    const { firstname, lastname, subject } = req.body;
+    const ip = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
+    let hoy = new Date();
+    let horas = hoy.getHours();
+    let fecha = hoy.getDate() + '-' + (hoy.getMonth() + 1) + '-' + hoy.getFullYear() + '' + '/' + '' + horas;
+
+    const response_key = req.body["g-recaptcha-response"];
+    const secret_key = process.env.RECAPTCHAPRIVATE;
+    const url =
+      `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+
+    fetch(url, {
+      method: "post",
+    })
+      .then((response) => response.json())
+      .then((google_response) => {
+
+        if (google_response.success == true) {
+
+
+          // Import the Nodemailer library
+
+
+          // Create a transporter object
+          const transporter = nodemailer.createTransport({
+            host: 'smtp.hostinger.com',
+            port: 465,
+            secureConnection: false, // use SSL
+            auth: {
+              user: process.env.CORREO,
+              pass: process.env.CONTRASENA,
+            }
+          });
+
+          // Configure the mailoptions object
+          const mailOptions = {
+            from: process.env.CORREO,
+            to: 'programacion2ais@dispostable.com',
+            subject: 'Contact information',
+            text: `
+                   <h1>Welcome!</h1>
+                   <p>Nombre: ${firstname}</p>
+                   <p>Correo: ${lastname}</p>
+                   <p>Fecha/Hora: ${fecha}</p>
+                   <p>Pais: ${pais}</p>
+                   <p>Ip: ${ip}</p>`
+          };
+
+          // Send the email
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              this.model.guardar(firstname, lastname, subject, ip, fecha, pais);
+              return res.send({ response: "Contact save and email successfully" });
+            }
+          });
+        } else {
+          return res.send({ response: "Failed captcha" });
         }
-        console.log('Database created successfully')
+      })
+      .catch((error) => {
+        // Some error while verify captcha
+        return res.json({ error });
       });
-    }
-  
-    conectar(){
-      this.db.run('CREATE TABLE IF NOT EXISTS contactos(name VARCHAR(255), email VARCHAR(255), commentary TEXT,ip TEXT,date TEXT)');
+
+
+
+
+
   }
-    
-    guardar(nombre,correo,comentario,ip,fecha){
-      this.db.run("INSERT INTO contactos VALUES (?, ?, ?, ?, ?)", [nombre, correo, comentario, ip, fecha]);
-    }
-  
-  }
-  
-  
-class ContactosController{
-    constructor(){
-      this.model = new ContactosModel();
-      this.model.conectar();
-    }
-  
-    save(req,res){
-      const { firstname, lastname, subject} = req.body;
-      const ip = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
-      let hoy = new Date();
-      let horas = hoy.getHours();
-      let minutos = hoy.getMinutes();
-      let hora = horas + ':' + minutos;
-      let fecha = hoy.getDate() + '-' + (hoy.getMonth() + 1) + '-' + hoy.getFullYear() + '' + '/' + '' + hora;
-      this.model.guardar(firstname,lastname,subject,ip,fecha);
-      res.send('Contact save successfully')
-    }
-  }
-  
+}
+
 
 module.exports = ContactosController
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const ContactosController = require('./controllers/models'); 
-
+require('dotenv').config()
 const miControlador = new ContactosController();
+
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', GP: process.env.RECAPTCHAPUBLIC });
 });
 
 
