d442f89af0a103f7101272a5cb47a5191ad61b32
yenderjose <167598978+yenderjose@users.noreply.github.com>
2024-06-24 05:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ passport.use(new LocalStrategy(
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  callbackURL: "https://yendercv.onrender.com/auth/github/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   return done(null, { username: profile.username });
